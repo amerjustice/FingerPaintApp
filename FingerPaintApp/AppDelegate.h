21a5d3658dd116ce07ea7a4351a74c515115bdfc
amerjustice <amerjustice@outlook.com>
2016-05-14 15:14:16 -0700

    Initial Commit

---
+++ FingerPaintApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FingerPaintApp
+//
+//  Created by Susan Justice on 2016-05-14.
+//  Copyright © 2016 Amer Justice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
